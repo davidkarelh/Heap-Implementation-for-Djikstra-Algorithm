142c26ddf0c698d355d53eba4c53a8ccd0894fd5
davidkarelh <88304364+davidkarelh@users.noreply.github.com>
2021-12-18 15:08:06 +0700

    Update

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -19,10 +19,10 @@ class Graph {
         // Doesn't fully delete a vertex from Graph, only delete 
         // the specified vertex from the adjacency list, 
         // deleted vertex may still exist in the remaining vertices routes
-        void delete_vertex(string in_name) {
+        void delete_vertex(string name) {
             int i, j;
             for (i = 0; i < length ; i++) {
-                if (adjacency_list[i].name == in_name) {
+                if (adjacency_list[i].name == name) {
                     for (j = i; j < length - 1; j++) {
                         adjacency_list[j] = adjacency_list[j + 1];
                     }
